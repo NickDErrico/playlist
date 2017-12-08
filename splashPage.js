e5b4a78e7654710140d01f6929adb8ee1c89823d
NickDErrico <nickderrico@hotmail.com>
2017-12-08 14:49:46 -0700

    added splashPage button functionality

--- splashPage.js
+++ splashPage.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 
-    $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
-      let albumLocation = $('.railroad-container .album')
-      let albumArt = []
-      for (let i = 0; i < data.results.length; i++) {
-        albumArt.push(data.results[i].cover_art);
-          for (let j = 0; j < albumLocation.length; j++) {
-            let randomImage = albumArt[Math.floor(Math.random() * albumArt.length)];
-            $(albumLocation[j]).attr("src", "images/" + randomImage)
-       }
+  $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
+    let albumLocation = $('.railroad-container .album')
+    let albumArt = []
+    for (let i = 0; i < data.results.length; i++) {
+      albumArt.push(data.results[i].cover_art);
+        for (let j = 0; j < albumLocation.length; j++) {
+          let randomImage = albumArt[Math.floor(Math.random() * albumArt.length)];
+          $(albumLocation[j]).attr("src", "images/" + randomImage)
      }
+   }
+})
+  $('#new-page').click(function() {
+    location.href = "playlistPage.html"  
   })
 })
