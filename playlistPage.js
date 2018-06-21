d3659d40b20c8c154a73360de83de6b37b44aa80
NickDErrico <nickderrico@hotmail.com>
2018-06-21 16:53:06 -0700

    added vanilla js

--- playlistPage.js
+++ playlistPage.js
@@ -1,30 +1,77 @@
-$(document).ready(function() {
+let topDiv = document.querySelector('.album-art-container')
+let middleDiv = document.querySelector('.album-info')
 
-  $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
-    let albumPosition = $('.album-showcase .albumArt')
-    for (let j = 0; j < albumPosition.length; j++) {
-      for (let i = 0; i < data.results.length; i++) {
-      let albumImage = data.results[i].cover_art;
-      let albumTitle = data.results[i].title;
-      if (i === j) {
-      $(albumPosition[j]).attr("src", "images/" + albumImage);
-      $(albumPosition[j]).attr("alt", albumTitle);
-        }
+fetch('https://lit-fortress-6467.herokuapp.com/object')
+  .then(function(data) {
+    return data.json()
+  }).then(function(results) {
+    let finalPlaylist = [];
+
+    for (let i = 0; i < results.results.length; i++) {
+      let newImg = document.createElement('img')
+      newImg.setAttribute('src', `./images/${results.results[i].cover_art}`);
+      newImg.setAttribute('class', `album-art ${i}`)
+      newImg.addEventListener('click', addToMiddle)
+      topDiv.appendChild(newImg)
+
+      function addToMiddle() {
+        let albumInfo = document.createElement('p');
+        albumInfo.innerText = `${results.results[i].artist}: ${results.results[i].title}`;
+        middleDiv.appendChild(albumInfo)
+
+        finalPlaylist.push(`${results.results[i].artist}: ${results.results[i].title}`)
       }
     }
-  })
 
-  $('.albumArt').click(function () {
-      let albumCover = $(this).attr("alt")
-      console.log(albumCover);
-      let albumTitle = this.alt;
-      let artistObject = {}
-      $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
-         for (let i = 0; i < data.results.length; i++) {
-            if (albumCover == data.results[i].title) {
-              $('.album-info').append("<p>" + data.results[i].artist + ": " + data.results[i].title + "</p>")
-        }
-      }
+    let clearButton = document.querySelector('.clear-tracks')
+    clearButton.addEventListener('click', function() {
+      middleDiv.innerText = '';
+      finalPlaylist = []
+    });
+
+    let submitButton = document.querySelector('.submit-bin')
+    submitButton.addEventListener('click', function(){
+      let httpRequest = new XMLHttpRequest()
+      httpRequest.open('POST', 'https://lit-fortress-6467.herokuapp.com/post', true);
+      httpRequest.send(finalPlaylist);
+      httpRequest.onreadystatechange = function()
+        {
+            if (httpRequest.readyState == 4)
+                if (httpRequest.status == 200)
+                console.log(httpRequest)
+        };
+
     })
-  })
-})
+
+});
+
+// $(document).ready(function() {
+//
+//   $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
+//     let albumPosition = $('.album-showcase .albumArt')
+//     for (let j = 0; j < albumPosition.length; j++) {
+//       for (let i = 0; i < data.results.length; i++) {
+//       let albumImage = data.results[i].cover_art;
+//       let albumTitle = data.results[i].title;
+//       if (i === j) {
+//       $(albumPosition[j]).attr("src", "images/" + albumImage);
+//       $(albumPosition[j]).attr("alt", albumTitle);
+//         }
+//       }
+//     }
+//   })
+//
+//   $('.albumArt').click(function () {
+//       let albumCover = $(this).attr("alt")
+//       console.log(albumCover);
+//       let albumTitle = this.alt;
+//       let artistObject = {}
+//       $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
+//          for (let i = 0; i < data.results.length; i++) {
+//             if (albumCover == data.results[i].title) {
+//               $('.album-info').append("<p>" + data.results[i].artist + ": " + data.results[i].title + "</p>")
+//         }
+//       }
+//     })
+//   })
+// })
--- splashPage.js
+++ splashPage.js
@@ -1,17 +1,61 @@
-$(document).ready(function() {
+let topAlbum = document.getElementById('one')
+let middleAlbum = document.getElementById('two')
+let bottomAlbum = document.getElementById('three')
 
-  $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
-    let albumLocation = $('.railroad-container .album')
-    let albumArt = []
-    for (let i = 0; i < data.results.length; i++) {
-      albumArt.push(data.results[i].cover_art);
-        for (let j = 0; j < albumLocation.length; j++) {
-          let randomImage = albumArt[Math.floor(Math.random() * albumArt.length)];
-          $(albumLocation[j]).attr("src", "images/" + randomImage)
-     }
-   }
-})
-  $('#new-page').click(function() {
-    location.href = "playlistPage.html"  
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+fetch('https://lit-fortress-6467.herokuapp.com/object')
+  .then(function(data) {
+    return data.json()
+  }).then(function(results) {
+    let random1 = getRandom(0,4)
+    let random2 = getRandom(0,4)
+    let random3 = getRandom(0,4)
+
+    topAlbum.setAttribute('src', `./images/${results.results[random1].cover_art}`)
+
+    if (random2 !== random1) {
+      middleAlbum.setAttribute('src', `./images/${results.results[random2].cover_art}`)
+    }else {
+      for(let i=0; i<5; i++){
+        if(random1 !== i){
+          random2 = i;
+          middleAlbum.setAttribute('src', `./images/${results.results[random2].cover_art}`)
+        }
+      }
+    }
+    if(random3 !== random2 && random3 !== random1) {
+      bottomAlbum.setAttribute('src', `./images/${results.results[random3].cover_art}`)
+    }else {
+      for(let j=0; j<5; j++){
+        if(random1 !== j && random2 !== j){
+          random3 = j
+          bottomAlbum.setAttribute('src', `./images/${results.results[random3].cover_art}`)
+        }
+      }
+    }
   })
-})
+
+
+
+// $(document).ready(function() {
+//
+//   $.get('https://lit-fortress-6467.herokuapp.com/object', function(data) {
+//     let albumLocation = $('.railroad-container .album')
+//     let albumArt = []
+//     for (let i = 0; i < data.results.length; i++) {
+//       albumArt.push(data.results[i].cover_art);
+//         for (let j = 0; j < albumLocation.length; j++) {
+//           let randomImage = albumArt[Math.floor(Math.random() * albumArt.length)];
+//           $(albumLocation[j]).attr("src", "images/" + randomImage)
+//      }
+//    }
+// })
+//   $('#new-page').click(function() {
+//     location.href = "playlistPage.html"
+//   })
+// })
